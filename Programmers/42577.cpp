5af9d7d125c0ba607992c640c9822ae188da1a54
jotmanggu <whalsrnwkd@naver.com>
2022-01-12 23:39:12 +0900

    220112

---
+++ Programmers/42577.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    set<string> s;
+    vector<string> v;
+    for (auto t : phone_book) {
+        s.insert(t);
+    }
+    for (auto t : s) {
+        v.push_back(t);
+    }
+    for (int i = 0; i < v.size() - 1; i++) {
+        if (v[i + 1].find(v[i]) == 0) {
+            answer = false;
+        }
+    }
+    return answer;
+}
---
+++ Programmers/42578.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    map<string, int> m;
+    for (auto t : clothes) {
+        m[t[1]]++;
+    }
+    for (auto t : m) {
+        answer *= (t.second + 1);
+    }
+    answer--;
+    return answer;
+}
