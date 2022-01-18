62c6fa36e30c5b521ae2caf0bb2966c0982cca9b
jotmanggu <whalsrnwkd@naver.com>
2022-01-18 23:20:14 +0900

    20220118

---
+++ BOJ/1620(failed).cpp
@@ -0,0 +1,43 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+bool isNumber(const string& str) {
+	for (char const& c : str) {
+		if (isdigit(c) == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(void) {
+	vector<string> v;
+	vector<string> v2;
+	string tmp;
+	int N, M;
+	int pokeNum;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+	for (int i = 0; i < M; i++) {
+		cin >> tmp;
+		v2.push_back(tmp);
+	}
+	for (int i = 0; i < M; i++) {
+		if (isNumber(v2[i]) == true) {
+			pokeNum = stoi(v2[i]);
+			cout << v[pokeNum - 1] << '\n';
+		}
+		else {
+			auto it = find(v.begin(), v.end(), v2[i]);
+			cout << it - v.begin() + 1 << '\n';
+		}
+	}
+	return 0;
+}
