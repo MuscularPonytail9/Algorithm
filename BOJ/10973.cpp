69a90465feee8687edaddee4b932f2ad07e115f5
jotmanggu <whalsrnwkd@naver.com>
2022-01-27 22:10:55 +0900

    20220127

---
+++ BOJ/10973.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+vector<int>v;
+
+int main(void) {
+    int N, temp;
+    cin >> N;
+    int current = 0;
+    for (int i = 0; i < N; i++) {
+        cin >> temp;
+        v.push_back(temp);
+    }
+    if (prev_permutation(v.begin(), v.end())) {
+        for (int i = 0; i < N; i++) {
+            cout << v[i] << ' ';
+        }
+    }
+    else {
+        cout << -1;
+    }
+    return 0;
+}
