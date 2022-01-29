daa70342918d6780d4fd919b6c373c557336159a
jotmanggu <whalsrnwkd@naver.com>
2022-01-29 23:26:18 +0900

    20220129

---
+++ BOJ/10971.cpp
@@ -0,0 +1,49 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+vector<int>v;
+vector<vector<int>>w;
+vector<int>t;
+int main(void) {
+    int N, temp;
+    cin >> N;
+    int MIN = 11000000;
+    int path = 0;
+    for (int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i++) {
+            cin >> temp;
+            t.push_back(temp);
+        }
+        w.push_back(t);
+        t.clear();
+    }
+    for (int i = 0; i < N; i++) {
+        v.push_back(i);
+    }
+    do {
+        for (int i = 0; i < N-1; i++) {
+            if (w[v[i]][v[i + 1]] != 0) {
+                path += w[v[i]][v[i + 1]];
+            }
+            else {
+                path = 11000000;
+            }
+        }
+        if (w[v[N - 1]][v[0]] != 0) {
+            path += w[v[N - 1]][v[0]];
+        }
+        else {
+            path = 11000000;
+        }
+        if (MIN > path) {
+            MIN = path;
+        }
+        path = 0;
+    } while (next_permutation(v.begin(), v.end()));
+    cout << MIN;
+    return 0;
+}
---
+++ BOJ/10974.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+vector<int>v;
+int main(void) {
+    int N, temp;
+    cin >> N;
+    for (int i = 1; i < N+1; i++) {
+        v.push_back(i);
+    }
+    do {
+        for (int i = 0; i < N; i++) {
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+    } while (next_permutation(v.begin(), v.end()));
+    return 0;
+}
