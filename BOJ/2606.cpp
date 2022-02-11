3fec8b009d9cf55241f2d0e650675ace32aa0f44
CHOMINGU <whalsrnwkd@naver.com>
2022-02-11 19:08:11 +0900

    20220211

---
+++ BOJ/2606.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <string> 
+#include <cmath>
+#include <cstdio>
+#include <set>
+
+using namespace std;
+
+
+int m[102][102];
+bool v[102]; 
+int N, M;
+int t1, t2;
+int num;
+void bfs() {
+    queue<int> q;
+    int current = 1;
+    q.push(1);
+    v[1] = true;
+    while (!q.empty()) {
+        current = q.front();
+        q.pop();
+        for (int i = 1; i < N + 1; i++) {
+            if (m[current][i] == 1 && v[i] == false) {
+                q.push(i);
+                v[i] = true;
+                num++;
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+
+    for (int i = 0; i < M; i++) {
+        cin >> t1 >> t2;
+        m[t1][t2] = 1;
+        m[t2][t1] = 1;
+    }
+
+    bfs();
+
+    cout << num;
+    return 0;
+}
---
+++ BOJ/2667.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <string> 
+#include <cmath>
+#include <cstdio>
+#include <set>
+
+using namespace std;
+
+int di[4] = { -1,0,1,0 }; 
+int dj[4] = { 0,1,0, -1 };
+
+vector<int> res;
+int map[27][27];
+bool v[27][27]; 
+int N;
+int num = 0;
+void bfs() {
+    queue<pair<int, int>> q;
+    for (int i = 1; i < N + 1; i++) {
+        for (int j = 1; j < N + 1; j++) {
+            if (v[i][j] == false && map[i][j] == 1) {
+                q.push({ i,j });
+                v[i][j] = true;
+                num++;
+                while (!q.empty()) {
+                    auto t = q.front();
+                    q.pop();
+                    int ti = t.first;
+                    int tj = t.second;
+
+                    for (int k = 0; k < 4; k++) {
+                        int ni = ti + di[k];
+                        int nj = tj + dj[k];
+                        if (ni <= 0 || nj <= 0 || ni > N || nj > N || map[ni][nj] == 0 || v[ni][nj]==true) continue;
+
+                        q.push({ ni,nj });
+                        v[ni][nj] = true;
+                        num++;
+                    }
+                }
+                res.push_back(num);
+                num = 0;
+            }
+        }
+    }
+    
+}
+
+int main() {
+    cin >>N;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            scanf("%1d", &map[i][j]);
+        }
+    }
+
+    bfs();
+    sort(res.begin(), res.end());
+    cout << res.size() << '\n';
+    for (auto k : res) {
+        cout << k << '\n';
+    }
+
+    return 0;
+}
