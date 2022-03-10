0dff75f4df8bd1314d1c3ae40b28c43f6b39a124
CHOMINGU <whalsrnwkd@naver.com>
2022-03-10 23:02:21 +0900

    20220310

---
+++ BOJ/1463(failed).cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <string> 
+#include <cmath>
+#include <cstdio>
+#include <set>
+
+using namespace std;
+
+
+int N;
+int cnt;
+int num = 1;
+void makeOne() {
+    if (num == N) {
+        cout << cnt;
+        return;
+    }
+    if (num * 3 <= N) {
+        num = num * 3;
+        cnt++;
+        makeOne();
+    }
+    else if (num * 2 <= N) {
+        num = num * 2;
+        cnt++;
+        makeOne();
+    }
+    else {
+        num = num + 1;
+        cnt++;
+        makeOne();
+    }
+}
+
+int main() {
+    cin >> N;
+    makeOne();
+    return 0;
+}
