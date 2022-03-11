ff1caf0ace5523d1d8fc4434f9e3ccf345c26f70
CHOMINGU <whalsrnwkd@naver.com>
2022-03-12 08:33:09 +0900

    20220312

---
+++ BOJ/1003(DP_Study).cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+/*
+ * [245] 백준 1003번 피보나치 함수
+ */
+
+using namespace std;
+
+#define MAX 40
+
+pair<int, int> dp[MAX];
+
+int main() {
+    int T, N;
+    cin >> T;
+    dp[0] = { 1, 0 };
+    dp[1] = { 0, 1 };
+
+    for (int i = 2; i <= MAX; ++i) {
+        dp[i] = { dp[i - 1].first + dp[i - 2].first, dp[i - 1].second + dp[i - 2].second };
+    }
+
+    while (T--) {
+        cin >> N;
+        cout << dp[N].first << " " << dp[N].second << "\n";
+    }
+    return 0;
+}
---
+++ BOJ/11726.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <string> 
+#include <cmath>
+#include <cstdio>
+#include <set>
+
+using namespace std;
+int N;
+int dp[1002];
+int main(void) {
+	cin >> N;
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 3;
+	dp[4] = 5;
+	for (int i = 5; i <= N; i++) {
+			dp[i] = (dp[i-1] + dp[i - 2]) % 10007;
+	}
+	cout << dp[N] % 10007;
+	return 0;
+}
