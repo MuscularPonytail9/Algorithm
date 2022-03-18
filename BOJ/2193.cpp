406507aca09ad89469d9883f09423821f2abbc67
CHOMINGU <whalsrnwkd@naver.com>
2022-03-18 23:13:50 +0900

    20220318

---
+++ BOJ/2193.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <string> 
+#include <cmath>
+#include <cstdio>
+#include <set>
+
+using namespace std;
+
+int N;
+long long dp[92];
+
+int main(void) {
+	cin >> N;
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 1;
+	for (int i = 3; i <= N; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	cout << dp[N];
+	return 0;
+}
