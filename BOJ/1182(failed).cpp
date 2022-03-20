78ed3f480ec972dc60e4149662fee467a27e2ae4
CHOMINGU <whalsrnwkd@naver.com>
2022-03-20 23:32:47 +0900

    20220320

---
+++ BOJ/1182(failed).cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <string> 
+#include <cmath>
+#include <cstdio>
+#include <set>
+
+using namespace std;
+
+int N, S;
+int sy[21];
+int sum;
+int cnt;
+bool isFirst = true;
+bool v[21];
+void bt(int index) {
+	cout << "index is " << index << '\n';
+	if (index >= N) {
+		return;
+	}
+	if (isFirst == true) {
+		isFirst = false;
+		v[index] = true;
+		sum += sy[index];
+		cout << sum << '\n';
+		if (sum == S) {
+			cnt++;
+			cout << "cnt++" << '\n';
+		}
+		bt(index + 1);
+		v[index] = false;
+	}
+	else {
+		v[index] = true;
+		sum += sy[index];
+		cout << sum << '\n';
+		if (sum == S) {
+			cnt++;
+			cout << "cnt++" << '\n';
+		}
+		
+		bt(index + 1);
+		v[index] = false;
+	}
+}
+
+int main(void) {
+	cin >> N >> S;
+	for (int i = 0; i < N; i++) {
+		cin >> sy[i];
+	}
+	for (int i = 0; i < N; i++) {
+		cout << "bt(" << i << ") started" << '\n';
+		bt(i);
+		sum = 0;
+		isFirst = true;
+	}
+	cout << cnt;
+	return 0;
+}
