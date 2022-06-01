e1dc3ab6ee2f9c6c6d21c56eb07ab5764babb7db
MuscularPonytail9 <whalsrnwkd@naver.com>
2022-06-01 23:27:03 +0900

    20220601

---
+++ BOJ/11000.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <string> 
+#include <cmath>
+#include <cstdio>
+#include <set>
+#include <map>
+#include <functional>
+
+using namespace std;
+bool isEmpty;
+int N, m, classRoom, result;
+set<int> nums;
+map<int, int> S;
+map<int, int> F;
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int temp;
+        cin >> temp;
+        nums.insert(temp);
+        S[temp]++;
+        cin >> temp;
+        nums.insert(temp);
+        m = max(m, temp);
+        F[temp]++;
+    }
+    for (auto t : nums) {
+        classRoom = classRoom + S[t] - F[t];
+        result = max(result, classRoom);
+    }
+    cout << result;
+
+    return 0;
+}
---
+++ BOJ/11399.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cstring>
+#include <string> 
+#include <cmath>
+#include <cstdio>
+#include <set>
+#include <map>
+
+using namespace std;
+int N, result;
+vector<int> v;
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+    sort(v.begin(), v.end());
+    for (int i = 0; i < v.size(); i++) {
+        result += v[i] * (v.size() - i);
+    }
+    cout << result;
+
+    return 0;
+}
