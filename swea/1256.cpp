f3472f296c0f87c54aaf0227a9fbd6fc7e98c24d
MuscularPonytail9 <whalsrnwkd@naver.com>
2022-08-27 23:53:03 +0900

    20220827

---
+++ swea/1256.cpp
@@ -0,0 +1,61 @@
+﻿#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+#include <queue>
+#include <map>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <cstring>
+#include <climits>
+
+using namespace std;
+
+string arr[500];
+string ans;
+int N, idx;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	//freopen("sample_input.txt", "r", stdin);
+	int test_case;
+	int T;
+
+	cin >> T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		cin >> N;
+		N--;
+		string s;
+		cin >> s;
+		idx = 0;
+		for (int i = 0; i < s.length(); i++) {
+			string temp = "";
+			for (int j = i; j < s.length(); j++) {
+				temp = temp + s[j];
+			}
+			arr[idx++] = temp;
+		}
+		sort(arr, arr + idx);
+		if (N >= idx) {
+			ans = "none";
+		}
+		else {
+			ans = arr[N];
+		}
+		cout << "#" << test_case << " " << ans <<"\n";
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
---
+++ swea/3813.cpp
@@ -0,0 +1,70 @@
+﻿#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+#include <queue>
+#include <map>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <cstring>
+#include <climits>
+
+using namespace std;
+
+int W[200000], S[200000];
+int N, K;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	freopen("sample_input.txt", "r", stdin);
+	int test_case;
+	int T;
+
+	cin >> T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		cin >> N >> K;
+		for (int i = 0; i < N; i++) {
+			cin >> W[i];
+		}
+		for (int i = 0; i < K; i++) {
+			cin >> S[i];
+		}
+		int low = 0;
+		int high = 200000;
+		while (low < high) {
+			bool check = false;
+			int mid = (low + high) / 2;
+			int num = -1;
+			for (int i = 0; i < K; i++) {
+				for (int j = 0; j < S[i]; j++) {
+					num++;
+					if (num == N) {
+						low = mid + 1;
+						check = true;
+					}
+					if (W[num] > mid) {
+						i--;
+						break;
+					}
+				}
+			}
+			if (check == false) {
+				high = mid;
+			}
+		}
+		cout << "#" << test_case << " " << high << '\n';
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
