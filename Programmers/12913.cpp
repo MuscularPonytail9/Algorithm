4cd4fd3a03344c86dc7eb3343d8cc56d6f066c12
MuscularPonytail9 <whalsrnwkd@naver.com>
2022-09-15 21:51:06 +0900

    20220915

---
+++ Programmers/12913.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int dp[100001][4];
+
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+    for (int i = 0; i < 4; i++) {
+        dp[0][i] = land[0][i];
+    }
+    for (int i = 1; i < land.size(); i++) {
+        for (int j = 0; j < 4; j++) {
+            int maxi = 0;
+            for (int k = 0; k < 4; k++) {
+                if (j == k) {
+                    continue;
+                }
+                if (maxi < dp[i - 1][k]) {
+                    maxi = dp[i - 1][k];
+                }
+            }
+            dp[i][j] = land[i][j] + maxi;
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        if (answer < dp[land.size() - 1][i]) {
+            answer = dp[land.size() - 1][i];
+        }
+    }
+
+    return answer;
+}
