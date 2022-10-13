2881a2ffc19972cdd4281a6b5be198054f97f1a6
MuscularPonytail9 <whalsrnwkd@naver.com>
2022-10-14 00:18:26 +0900

    20221014

--- BOJ/14502.cpp
+++ BOJ/14502.cpp
@@ -7,21 +7,24 @@
 
 using namespace std;
 int m[9][9];
-bool visited[9][9];
 int dx[4] = { 0, 1, 0, -1 };
 int dy[4] = { -1, 0, 1, 0 };
 int maxi = 0;
 vector<pair<int, int>> v;
+vector<pair<int, int>> v2;
 
 int main() {
     int N, M;
     cin >> N >> M;
-    for (int i = 1; i <= M; i++) {
-        for (int j = 1; j <= N; j++) {
+    for (int j = 1; j <= N; j++) {
+        for (int i = 1; i <= M; i++) {
             int temp;
             cin >> temp;
             if (temp == 0) {
                 v.push_back({ i, j });
+            }
+            else if (temp == 2) {
+                v2.push_back({ i, j });
             }
             m[i][j] = temp;
         }
@@ -30,45 +33,40 @@ int main() {
         for (int sec = fir + 1; sec < v.size() - 1; sec++) {
             for (int thi = sec + 1; thi < v.size(); thi++) {
                 int tempm[9][9];
+                bool visited[9][9];
                 for (int i = 0; i < 9; i++) {
                     for (int j = 0; j < 9; j++) {
                         tempm[i][j] = m[i][j];
+                        visited[i][j] = false;
                     }
                 }
                 tempm[v[fir].first][v[fir].second] = 1;
                 tempm[v[sec].first][v[sec].second] = 1;
                 tempm[v[thi].first][v[thi].second] = 1;
-                for (int i = 1; i <= M; i++) {
-                    for (int j = 1; j <= N; j++) {
-                        if (tempm[i][j] != 2 || visited[i][j] == true) {
-                            continue;
-                        }
-                        queue<pair<int, int>> q;
-                        q.push({ i, j });
-                        while (q.empty() == false) {
-                            auto cur = q.front();
-                            int cx = cur.first;
-                            int cy = cur.second;
-                            tempm[cx][cy] = 2;
-                            visited[cx][cy] = true;
-                            q.pop();
-                            for (int k = 0; k < 4; k++) {
-                                int nx = cx + dx[k];
-                                int ny = cy + dy[k];
-                                if (tempm[nx][ny] == 1 || visited[nx][ny] == true || nx < 1 || nx > M || ny < 1 || ny > N) {
-                                    continue;
-                                }
-                                q.push({ nx, ny });
-                            }
-                            for (int p = 1; p <= N; p++) {
-                                for (int o = 1; o <= M; o++) {
-                                    cout << tempm[o][p] << " ";
-                                }
-                                cout << '\n';
-                            }
-                            cout << '\n';
-                        }
+                for (int u = 0; u < v2.size(); u++) {
+                    int i = v2[u].first;
+                    int j = v2[u].second;
+                    if (visited[i][j] == true) {
+                        continue;
                     }
+                    queue<pair<int, int>> q;
+                    q.push({ i, j });
+                    while (q.empty() == false) {
+                        auto cur = q.front();
+                        int cx = cur.first;
+                        int cy = cur.second;
+                        tempm[cx][cy] = 2;
+                        visited[cx][cy] = true;
+                        q.pop();
+                        for (int k = 0; k < 4; k++) {
+                            int nx = cx + dx[k];
+                            int ny = cy + dy[k];
+                            if (tempm[nx][ny] == 1 || tempm[nx][ny] == 2 || visited[nx][ny] == true || nx < 1 || nx > M || ny < 1 || ny > N) {
+                                continue;
+                            }
+                            q.push({ nx, ny });
+                        }
+                    }                   
                 }
                 int cnt = 0;
                 for (int i = 1; i <= M; i++) {
@@ -78,7 +76,6 @@ int main() {
                         }
                     }
                 }
-                cout << cnt << '\n';
                 if (maxi < cnt) {
                     maxi = cnt;
                 }
